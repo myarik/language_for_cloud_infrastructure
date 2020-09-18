20b4242a13b8dff668fb9722c05bb2e87aa25f20
Yaroslav Muravskyi <y@myarik.com>
2020-09-18 13:36:14 +0200

    Refactor the first_response.py

--- python-demo/python_demo/first_response.py
+++ python-demo/python_demo/first_response.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import sys
 import time
 from typing import Optional
 
@@ -7,13 +8,15 @@ import aiohttp
 import click
 import uvloop
 from attr import dataclass
+from loguru import logger
 
-HOST_URL = os.environ.get("API_HOST_URL")
+logger.remove()
+logger.add(sys.stderr, level="INFO")
 
 STORAGE_MAPPING = {
-    "storage0": "0cf50f1c99234954b00340471538ce9d.MOV",
-    "storage1": "0db9a58b669048dc999eb8f11f7ba424.MOV",
-    "storage2": "0d38ceda70b14ccfaf6960514615757f.MOV",
+    "storage0": "http://212.183.159.230/10MB.zip",
+    "storage1": "http://ipv4.download.thinkbroadband.com/10MB.zip",
+    "storage2": "http://speedtest.tele2.net/10MB.zip",
 }
 
 
@@ -28,10 +31,9 @@ async def replica_storage(storage: str) -> Result:
     """
     Download a content
     """
-    click.secho(f"Begin downloading from the {storage}", fg="yellow")
-    url = f"{HOST_URL}{STORAGE_MAPPING[storage]}"
+    logger.debug(f"Begin downloading {storage}")
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
+        async with session.get(STORAGE_MAPPING[storage]) as resp:
             return Result(storage, content=await resp.read())
 
 
@@ -52,6 +54,12 @@ async def async_main() -> None:
     Send requests to multiple replicas, and use the first response.
     """
     s = time.perf_counter()
+
+    is_debug_level = os.environ.get("DEBUG", False)
+    if is_debug_level:
+        logger.remove()
+        logger.add(sys.stderr, level="DEBUG")
+
     tasks = [
         asyncio.create_task(replica_storage(f"storage{index}")) for index in range(3)
     ]
@@ -63,14 +71,12 @@ async def async_main() -> None:
     for task in done:
         try:
             result = task.result()
-            click.secho(
-                f"{result.storage.title()} returns the first result", fg="green"
-            )
+            logger.info(f"{result.storage.title()} returns the first result")
         except Exception as e:
-            click.secho(f"Task returns an error {repr(e)}", fg="red")
+            logger.error(f"Task returns an error {repr(e)}")
         break
     else:
-        click.secho(f"Timeout error", fg="red")
+        logger.error("Timeout error")
 
     # Cancel pending tasks
     for task in pending:
@@ -79,7 +85,7 @@ async def async_main() -> None:
     await asyncio.gather(*pending, return_exceptions=True)
 
     elapsed = time.perf_counter() - s
-    click.secho(f"Execution time: {elapsed:0.2f} seconds.", fg="bright_blue")
+    logger.info(f"Execution time: {elapsed:0.2f} seconds.")
 
 
 if __name__ == "__main__":
